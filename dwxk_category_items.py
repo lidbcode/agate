897da9bba0e6d73f1fd911e56514ca0ef9d43fb2
lidbwork@163.com <lidbwork@163.com>
2017-07-28 15:38:39 +0800

    update

--- dwxk_category_items.py
+++ dwxk_category_items.py
@@ -11,9 +11,9 @@ import time
 def index():
     url = "https://www.daweixinke.com/sqe.php"
     items = []
-    for c1 in range(-14,0):
+    for c1 in range(-1,-15,-1):
         parameter = "?s=/CCKItem/getAdItemList&data[platform]=web&data[pageNum]=10&data[order_status]=10&data[c1]=%s&data[page]="%(c1,)
-        for page in range(1,50):
+        for page in range(1,101):
             response = urllib.urlopen(url + parameter + str(page))
             ## print url + parameter + str(page) 
             result = json.load(response)
@@ -40,7 +40,7 @@ def getCoupon(data):
         'Accept-Language': 'zh-CN,zh;q=0.8',
         'Content-Type': 'application/x-www-form-urlencoded',
         'charset': 'UTF-8',
-        'Cookie':'UM_distinctid=15cafebfdc158-0f5d0448a59178-323f5c0f-1fa400-15cafebfdc2790; m_cck_access_token=54AC5F8C8B8B93CE107B95BB46E8D9D8; CNZZDATA1261674589=503176748-1499417870-https%253A%252F%252Fwww.daweixinke.com%252F%7C1499417870; CNZZDATA1261330578=1564621818-1499414616-%7C1499845721; cck_access_token=10B3EBD7ABC237329ABB699F9074B97D; PHPSESSID=15n4ms5eb1ghogsvkcml6blo07; gwapp_userId=10360; CNZZDATA1261674382=1815685325-1499077465-https%253A%252F%252Fwww.daweixinke.com%252F%7C1500538419; CNZZDATA1261294143=450722296-1497595338-https%253A%252F%252Fwww.google.co.jp%252F%7C1500534407'
+        'Cookie' : 'UM_distinctid=15cafebfdc158-0f5d0448a59178-323f5c0f-1fa400-15cafebfdc2790; m_cck_access_token=54AC5F8C8B8B93CE107B95BB46E8D9D8; CNZZDATA1261674589=503176748-1499417870-https%253A%252F%252Fwww.daweixinke.com%252F%7C1499417870; CNZZDATA1261330578=1564621818-1499414616-%7C1499845721; CNZZDATA1261674382=1815685325-1499077465-https%253A%252F%252Fwww.daweixinke.com%252F%7C1500538419; gwapp_userId=10360; PHPSESSID=re0fs5fi4mij7dkjti95sm23m4; cookie_test=1; cck_access_token=A36FF87B177F17EACC68150E55C3C719; CNZZDATA1261294143=450722296-1497595338-https%253A%252F%252Fwww.google.co.jp%252F%7C1501223559'
     }
     req = urllib2.Request(url, data, headers)
     response = urllib2.urlopen(req)
