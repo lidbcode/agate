4de51136269b13d8c40e23dc3578f21d1d58f1e3
lidb <lidbwork@163.com>
2017-08-20 21:09:41 +0800

    update conf

---
+++ qing/analysis.py
@@ -0,0 +1,55 @@
+# -*- coding: UTF-8 -*-
+
+import time
+import json
+
+
+def work():
+    ds = time.strftime("%Y%m%d", time.localtime())
+    res_dic = {}
+    for r in range(1, 3):
+        fin = open("coupon-%s-%s.json" % (ds, r))
+        for line in fin:
+            line_json = json.loads(line)
+            item_id = line_json["itemId"]
+            activity_id = line_json["activityId"]
+            spider_time = int(line_json["time"])
+
+            start_time = 0
+            end_time = spider_time
+
+            if line_json["effectiveStartTime"] != "":
+                start_time = int(line_json["effectiveStartTime"])
+            if line_json["effectiveEndTime"] != "":
+                end_time = int(line_json["effectiveEndTime"])
+
+            if (item_id, activity_id) not in res_dic:
+                res_dic[(item_id, activity_id)] = (start_time, end_time)
+            else:
+                (old_start_time, old_end_time) = res_dic[(item_id, activity_id)]
+                if old_start_time > start_time:
+                    start_time = old_start_time
+                if old_end_time < end_time:
+                    end_time = old_end_time
+                res_dic[(item_id, activity_id)] = (start_time, end_time)
+        fin.close()
+
+    freq_dic = {}
+    for (item_id, activity_id) in res_dic.keys():
+        start_time = res_dic[(item_id, activity_id)][0]
+        end_time = res_dic[(item_id, activity_id)][1]
+        online_time = (end_time - start_time) / 3600
+        if start_time != 0:
+            if online_time not in freq_dic:
+                freq_dic[online_time] = 1
+            else:
+                freq_dic[online_time] = freq_dic[online_time] + 1
+
+    fout = open("res.csv", "w")
+    for freq in sorted(freq_dic.keys()):
+        fout.write("%s,%s\n" % (freq, freq_dic[freq]))
+    fout.close()
+
+
+if __name__ == "__main__":
+    work()
---
+++ qing/spider_item.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+
+import re
+from selenium import webdriver
+import time
+import json
+
+
+def work():
+    driver = webdriver.Chrome("chromedriver.exe")
+    ds = time.strftime("%Y%m%d", time.localtime())
+    fout = open("data-%s.json" % (ds,), 'w')
+    result = []
+    for round in range(1, 101):
+        print "the %s page start " % (round,)
+
+        url = "http://www.qingtaoke.com/qingsou?istmall=1&min_price=50&s_type=1&sort=1&page=%s&f=1" % (round,)
+        driver.get(url)
+        page = driver.page_source
+        item_id_list = re.findall(">https://item.taobao.com/item\.htm\?id=([0-9]+)", page, re.S)
+        activity_id_list = re.findall("activityId=([A-Za-z0-9]*)<", page, re.S)
+
+        print "item_id_list size is %d , activity_id_list size is %d" % (len(item_id_list), len(activity_id_list))
+
+        url_head = "https://uland.taobao.com/coupon/edetail"
+        for i in range(0, min(len(item_id_list), len(activity_id_list))):
+            activity_id = activity_id_list[i]
+            item_id = item_id_list[i]
+            coupon_url = "%s?activityId=%s&itemId=%s" % (url_head, activity_id, item_id)
+            item_coupon = {"item_id": item_id, "coupon_url": coupon_url}
+            result.append(json.dumps(item_coupon))
+        print "the %s page end \n" % (round,)
+    fout.write("\n".join(result))
+    fout.close()
+    driver.quit()
+    print "work is OK"
+
+
+if __name__ == '__main__':
+    work()
+    print "success"
